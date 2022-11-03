ef173fe934a6916f3721294995527b646ff020ad
Luis <67877206+xiluisx@users.noreply.github.com>
2022-11-03 10:40:55 -0600

    Created math.Vector2

---
+++ pysics/math/Vector2.py
@@ -0,0 +1,6 @@
+class Vector2:
+    def __init__(self, x:float, y:float):
+        self.x = x
+        self.y = y
+
+    # TODO: Implementar suma, resta y multiplicacion de vectores con def __add__ def __sub__ y def __mul__
