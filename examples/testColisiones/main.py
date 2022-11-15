78f743f3f603a92b9fe9171febfd16adfb8d60e2
Luis <67877206+xiluisx@users.noreply.github.com>
2022-11-15 10:44:38 -0600

    Changed second ball color

--- examples/testColisiones/main.py
+++ examples/testColisiones/main.py
@@ -6,7 +6,7 @@ from pysics.physics.CircleCollider import CircleCollider
 game = GameHandler.Game("Test", 600, 800)
 game.start()
 obj = game.add_object(os.path.dirname(__file__) + "\img\RedBall.png", CircleCollider(10, Vector2(0,0)))
-obj2 = game.add_object(os.path.dirname(__file__) + "\img\RedBall.png", CircleCollider(10, Vector2(100,0)), Vector2(100,0))
+obj2 = game.add_object(os.path.dirname(__file__) + "\img\BlueBall.png", CircleCollider(10, Vector2(100,0)), Vector2(100,0))
 obj.col.object = obj
 obj2.col.object = obj2
 obj.add_aceleracion(Vector2(1, 0))
