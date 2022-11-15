ceb406a292633db437b06356d1e29915f2aa1d3a
CarlosMuLa <carlos.muniz@iteso.mx>
2022-11-15 09:49:21 -0600

    Implementacion de Colisiones

--- pysics/physics/CircleCollider.py
+++ pysics/physics/CircleCollider.py
@@ -1,3 +1,4 @@
+import math
 from pysics.math.Vector2 import Vector2
 
 
@@ -11,6 +12,12 @@ class CircleCollider:
         # TODO: Revisa que el otro objeto sea circulo o cuadrado
         if other is CircleCollider:
             # TODO: Mide distancia entre ambos
+            sumx = (self.x - other.x)**2
+            sumy = (self.y - other.y)**2
+            raiz = math.sqrt(sumx + sumy)
+
             # TODO: Si distancia es menor a ambos radios sumados estan chocando
+            if raiz < self.radius + other.radius:
+                return True
             return
         return False # Cambiar esto
