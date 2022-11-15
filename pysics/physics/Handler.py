88bd7c7a96ead683e9b6f8c1ecf2c1d678bee672
Luis <67877206+xiluisx@users.noreply.github.com>
2022-11-15 10:42:21 -0600

    Removed TODOs

--- pysics/physics/Handler.py
+++ pysics/physics/Handler.py
@@ -5,13 +5,12 @@ class Handler:
     def __init__(self):
         self.colliders = []
 
-    # TODO: Revisar colisiones dependiendo del tipo de collider que tenga
     def update(self):
         for col in self.colliders:
             for other in self.colliders:
                 if other == col:
                     continue
                 else:
-                    if col.is_colliding(other):  # TODO: Hacer que se choque cuando colisiones
+                    if col.is_colliding(other):
                         other.object.aceleracion += col.object.aceleracion
                         col.object.aceleracion = Vector2(-col.object.aceleracion.x, -col.object.aceleracion.y)
