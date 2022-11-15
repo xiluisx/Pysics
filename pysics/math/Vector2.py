3a09b7893a2b11942c9f82029a19f59a5a22eec9
Luis <67877206+xiluisx@users.noreply.github.com>
2022-11-15 10:32:17 -0600

    Removed TODO

--- pysics/math/Vector2.py
+++ pysics/math/Vector2.py
@@ -11,5 +11,3 @@ class Vector2:
 
     def __mul__(self, otro):
         return Vector2(self.x * otro.x, self.y * otro.y)
-
-    # TODO: Implementar suma, resta y multiplicacion de vectores con def __add__ def __sub__ y def __mul__
