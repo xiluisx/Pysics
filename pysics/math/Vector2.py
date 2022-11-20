2ae5ff253d56c47b9a439111d0db1fc6857f8bfc
Luis <67877206+xiluisx@users.noreply.github.com>
2022-11-20 11:50:11 -0600

    Added str and rotate methods

--- pysics/math/Vector2.py
+++ pysics/math/Vector2.py
@@ -1,3 +1,6 @@
+import math
+
+
 class Vector2:
     def __init__(self, x:float, y:float):
         self.x = x
@@ -11,3 +14,12 @@ class Vector2:
 
     def __mul__(self, otro):
         return Vector2(self.x * otro.x, self.y * otro.y)
+
+    def __str__(self):
+        return "x: " + str(self.x) + ", y:" + str(self.y)
+
+    def rotate(self, angle):
+        cos = math.cos(angle)
+        sin = math.sin(angle)
+        self.x = cos * self.x - sin * self.y
+        self.y = sin * self.x - cos * self.y
