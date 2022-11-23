2a3e07c72e5e9bd34ee15c6a0b89a48dacedd0bb
Luis <67877206+xiluisx@users.noreply.github.com>
2022-11-23 09:13:07 -0600

    Fixed tests

--- examples/testColisiones/main.py
+++ examples/testColisiones/main.py
@@ -8,6 +8,9 @@ game.start()
 obj = game.add_object(os.path.dirname(__file__) + "\img\RedBall.png", CircleCollider(10, Vector2(0,0)))
 obj2 = game.add_object(os.path.dirname(__file__) + "\img\BlueBall.png", CircleCollider(10, Vector2(100,0)), Vector2(100,0))
 obj.col.object = obj
+obj.name = "Bola Roja"
 obj2.col.object = obj2
+obj2.name= "bola Azul"
+obj2.col.mass = 20
 obj.add_aceleracion(Vector2(1, 0))
 
--- examples/testParedes/main.py
+++ examples/testParedes/main.py
@@ -1,4 +1,6 @@
 import os
+import random
+
 from pysics.game import GameHandler
 from pysics.math.Vector2 import Vector2
 from pysics.physics.CircleCollider import CircleCollider
@@ -6,11 +8,26 @@ from pysics.physics.SquareCollider import SquareCollider
 
 game = GameHandler.Game("Test", 600, 800)
 game.start()
-obj = game.add_object(os.path.dirname(__file__) + "\img\RedBall.png", CircleCollider(10, Vector2(0, 0)), Vector2(400,300))
-obj.col.object = obj
-pared1 = game.add_object(os.path.dirname(__file__) + "\img\Background.png", SquareCollider(Vector2(0, 0), 30,1000))
+pared1 = game.add_object(os.path.dirname(__file__) + "\img\Background.png", SquareCollider(Vector2(0,0), 30,1000))
 pared1.col.static = True
+pared1.col.object = pared1
+pared1.name = "pared"
 
 pared2 = game.add_object(os.path.dirname(__file__) + "\img\Background.png", SquareCollider(Vector2(0,500), 30, 1000), Vector2(0,500))
 pared2.col.static = True
-obj.add_aceleracion(Vector2(2, 10))
+pared2.col.object = pared2
+
+pared3 = game.add_object(os.path.dirname(__file__) + "\img\Background2.png", SquareCollider(Vector2(0,0), 1000, 30))
+pared3.col.static = True
+pared3.col.object = pared3
+
+pared4 = game.add_object(os.path.dirname(__file__) + "\img\Background2.png", SquareCollider(Vector2(800,0), 1000, 30), Vector2(800,0))
+pared4.col.static = True
+pared4.col.object = pared4
+
+for i in range(0,50):
+    randX = random.randint(400, 500)
+    randY = random.randint(200,300)
+    newObj = game.add_object(os.path.dirname(__file__) + "\img\RedBall.png", CircleCollider(10, Vector2(randX,randY)), Vector2(randX,randY))
+    newObj.col.object = newObj
+    newObj.add_aceleracion(Vector2(random.randint(0,10), random.randint(0,5)))
