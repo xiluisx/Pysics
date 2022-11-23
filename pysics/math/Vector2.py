7f7eba558d3e2c95eebd5fcc4934d4f79c69f14c
Luis <67877206+xiluisx@users.noreply.github.com>
2022-11-23 08:56:31 -0600

    Added multiple vector math functions

--- pysics/math/Vector2.py
+++ pysics/math/Vector2.py
@@ -15,6 +15,9 @@ class Vector2:
     def __mul__(self, otro):
         return Vector2(self.x * otro.x, self.y * otro.y)
 
+    def __neg__(self):
+        return Vector2(-self.x, -self.y)
+
     def __str__(self):
         return "x: " + str(self.x) + ", y:" + str(self.y)
 
@@ -23,3 +26,23 @@ class Vector2:
         sin = math.sin(angle)
         self.x = cos * self.x - sin * self.y
         self.y = sin * self.x - cos * self.y
+
+    @staticmethod
+    def dot_product(vec1, vec2):
+        return vec1.x * vec2.x + vec1.y * vec2.y
+
+    @staticmethod
+    def float_mult(float, vec):
+        return Vector2(vec.x * float, vec.y * float)
+
+    def magnitude(self):
+        return math.sqrt(self.x**2 + self.y**2)
+
+    def normalized(self):
+        if self.magnitude() == 0:
+            return Vector2(0,0)
+        return Vector2(self.x / self.magnitude(), self.y / self.magnitude())
+
+    @staticmethod
+    def clamp(vec, minVec, maxVec):
+        return Vector2(min(max(vec.x, minVec.x), maxVec.x), min(max(vec.y, minVec.y), maxVec.y))
